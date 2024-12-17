c42a4761a7fb53fe4623a9f398f6f737ac095fbc
Michael Bui <michaelbui99@hotmail.com>
2024-12-17 12:44:02 +0100

    Example gauge

--- app/src/main/java/dk/michaelbui/salling/foodwaste/prometheus/exporter/App.java
+++ app/src/main/java/dk/michaelbui/salling/foodwaste/prometheus/exporter/App.java
@@ -1,7 +1,30 @@
 package dk.michaelbui.salling.foodwaste.prometheus.exporter;
 
-public class App {
-    public static void main(String[] args) {
+import io.prometheus.metrics.core.metrics.Gauge;
+import io.prometheus.metrics.exporter.httpserver.HTTPServer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Logger logger = LogManager.getLogger();
+
+        HTTPServer server = HTTPServer.builder()
+                .port(8080)
+                .buildAndStart();
+
+
+        Gauge discounts = Gauge.builder()
+                .name("foodwaste_offcer")
+                .help("Current foodwaste offer")
+                .labelNames("product", "store")
+                .register();
+
+        discounts.labelValues("test", "åbyhøj").set(200);
+
+        logger.info("HTTP Server exposing endpoint /metrics on port {}", server.getPort());
+        Thread.currentThread().join();
     }
 }
